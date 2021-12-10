adde810e036b2395c36e19158d57cc1f76278beb
Islombek <islombek5958800@gmail.com>
2021-12-10 18:52:15 +0500

    toDoApp add style

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const todoContainer = document.querySelector(".todo__wrapper");
 const form = document.forms[0];
 const title = form.title;
-
+const add = document.querySelector(".add");
+const icon = document.querySelector(".icon");
+const toDoApp = document.querySelector(".toDoApp");
 const database = [];
 
 function Car(title) {
@@ -22,7 +24,8 @@ function display() {
   let htmlContent = '';
   for (let i = 0; i < database.length; i++) {
     htmlContent += `
-    <li data-id=${database[i].id} class="todo__item">${database[i].title}
+    <li data-id=${database[i].id} class="todo__item">
+      <p>${database[i].title}</p>
       <div class="edit__wrapper">
         <i data-id=${database[i].id} class="fas fa-pen" onclick="editToDo(this)"></i>
       </div>
@@ -31,7 +34,6 @@ function display() {
   }
   todoContainer.innerHTML = htmlContent;
 }
-
 
 function editToDo(e) {
   let id = e.parentElement.parentElement.dataset.id
@@ -58,7 +60,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   addToDo()
+  form.style.display = "none";
 })
 
+
+
+add.addEventListener("click",(event)=>{
+  event.preventDefault()
+  form.style.display = "block";
+})
+
+let x = 1;
+icon.addEventListener("click",event => {
+  event.preventDefault()
+  if (x==1){
+    toDoApp.style.display = "block";
+    x=0
+  }else{
+    toDoApp.style.display = "none";  
+    x=1 
+  }
+})
 
 
